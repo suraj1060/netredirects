4fc5f99b057399ffbd4e8672345a4726a738c810
Suraj Vishwakarma <surajv@solguruz.com>
2023-07-10 11:29:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+// import Blog from './pages/Blog'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/about"}>About</Link>
+      <Link to={"/contact"}>Contact</Link>
+      <Link to={"/blog"}>Blog</Link>
+      <Link to={"/blog/basic"}>Blog Menu 1</Link>
+      <h6>Redirect Netlifly</h6>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/*" element={<Blog />} /> */}
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
